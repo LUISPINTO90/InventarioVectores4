e61a80cfc898b1e1aa2d8b3dfca2582fdd5e4dad
LUISPINTO90 <lpinto1@ucol.mx>
2022-09-19 01:23:29 -0500

    Add README.md

---
+++ README.md
@@ -0,0 +1,14 @@
+<div align="center">
+      <h2>
+        <a
+          href="https://luispinto90.github.io/InventarioVectores/"
+          target="_blank"
+          >📝 CRUD INVENTARIO PRODUCTOS</a
+        >
+      </h2>
+      <br />
+</div>
+
+Elaborado con HTML, JavaScript, CSS.
+
+![Imagen](img/001.png)
--- src/product.js
+++ src/product.js
@@ -7,29 +7,4 @@ export default class Product {
     this.quantity = quantity;
     this.cost = cost;
   }
-
-  getCode() {
-    return this.code;
-  }
-
-  getName() {
-    return this.name;
-  }
-  setName(name) {
-    this.name = name;
-  }
-
-  getQuantity() {
-    return this.quantity;
-  }
-  setQuantity(quantity) {
-    this.quantity = quantity;
-  }
-
-  getCost() {
-    return this.cost;
-  }
-  setCost(cost) {
-    this.cost = cost;
-  }
 }
